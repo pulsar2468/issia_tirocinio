2c79f1a6546a7df130837773f4d2bc672fb2541c
Giuseppe La Tona <giulatona@gmail.com>
2018-03-06 18:29:18 +0100

    store_it updated

--- mqtt_client/store_it.py
+++ mqtt_client/store_it.py
@@ -1,82 +1,169 @@
-import paho.mqtt.client as paho
-import sqlite3
 import datetime
+
+import time
+
+from sys import exit
+
+import paho.mqtt.client as paho
+
+import struct
+
+import sqlite3
+
+
+global board_list
+
+board_list=[]
+
+def init_db():
+
+    #omissis
+
+    pass
+
+
+
+def add_to_db(buffer_b):
+
+    #omissis
+
+    pass
+
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 
+def hello(client, userdata, msg):
 
-def welcome(client, userdata, msg):
-    #name_board=(msg.topic).split('/', 1)[0]
-    board_list=[]
-    board_list.append(msg.payload)
-    print 'Hello client, i\'m ',msg.payload
+    board_name=(msg.topic).split('/', 1)[0]
+
+    board_list.append(msg.topic)
+
+    print("Hello client, this is ", board_name, " Answer: ", msg.payload)
+
+
 
 def store_data(client, userdata, msg):
-    #print((msg.payload))
     buffer_b=bytearray()
+
     buffer_b.extend(msg.payload)
-    import struct
-    x=struct.unpack_from('!ccccccccc',buffer_b,0)
-    print [ord(i) for i in x]
-    x=struct.unpack_from('!fffffffffff',buffer_b,9)
-    print [i for i in x]
+
+    #store new data
+
+    add_to_db(buffer_b)
+
+    #print new formatted data
+
+    board_name=(msg.topic).split('/', 1)[0]
+
+    board_id=ord(msg.payload[1])
+
+    board_type=ord(msg.payload[2])
+    date = '{:02d} {:02d} {:02d} {:02d} {:02d} {:02d}'.format(*[bcdToInt(i) for i in msg.payload[3:9]])
+    #date=msg.payload[3:9]
+
+    date = datetime.datetime.strptime(date,"%y %m %d %H %M %S")
 
 
-    #global package
-    #package=package+1
-    '''
-    name_board=(msg.topic).split('/', 1)[0] #I get the name of board
-    id_board=(msg.payload.split(' ')[1])
-    board_type=(msg.payload.split(' ')[2])
-    date=(msg.payload.split(' ')[3:9])
-    date=' '.join(date)
-    #Vmeans,Vrms,Imeans,Irms,A,P,T,f,wire,i2c,spi=\
-    Vmean3,Vmean2,Vmean1,Vmeans0=msg.payload.split(' ')[9:13]
-    elements=[Vmean3,Vmean2,Vmean1,Vmeans0]
-    buffer_b=bytearray(elements)
-    import struct
-    x=struct.unpack_from('f',buffer_b)
-    print x
-    '''
+    #buffer_b = bytearray(msg.payload[9:53])
+    (Vdc, Vrms, Idc, Irms, Pdc, P, A, T, wire, i2c, spi) = struct.unpack_from("!fffffffffff", buffer_b, 9)
+
+    fmtString = "{:.2f}\t" * 11
+    print("{} id: {} type: 0x{:0x} -- {}".format(board_name, board_id, board_type, date))
+    print(fmtString.format(Vdc, Vrms, Idc, Irms, Pdc, P, A, T, wire, i2c, spi))
 
 
 
-    #print(name_board, datetime.datetime.strptime(date,"%y %m %d %H %M %S"),
-          #Vmeans,Vrms,Imeans,Irms,A,P,T,f,wire,i2c,spi)
+def bcdToInt(bcdStr):
+    bcd, = struct.unpack('b', bcdStr)
+    bcdInt = (bcd >> 4) * 10 + (bcd & 0x0f)
+    return bcdInt
 
-    #date=(datetime.datetime.now())
-
-    '''
-    conn = sqlite3.connect('/home/nataraja/Scrivania/Issia&tesi/sweet_home.sqlite')
-    c = conn.cursor()
-    c.execute("INSERT or IGNORE INTO '%s' VALUES (%d,%d,'%s')" % (name_board,int(temperature),int(humidity),date) )
-
-    conn.commit()
-    conn.close()
-    '''
-
-
+#main code
 
 print("Store_it module active")
+
+print("discovering boards...")
+
 client = paho.Client()
 client.username_pw_set("issia", "cnr")
 client.connect("150.145.127.45", 8883)
-import struct
+
+
+
+
+
+client.subscribe("wemos0/hello",qos=0)
+
+client.message_callback_add("wemos0/hello", hello)
+
+
+
+#send a broadcast request on topic /requestHello
+
 x=struct.pack("ccc",chr(0x68),chr(0xFF),chr(0x00))
-client.publish("/requestHello",x,0)
+
+client.publish("/requestHello", x, 0)
+
+
+
+#check for answers
+
 client.loop_start()
+
+start=time.time()
+
+#allow 10 seconds for all wemos to respond
+
+while (time.time() - start < 10.0):
+
+   # print("waiting...")
+
+    pass
+
+
+
 client.loop_stop()
+
 client.disconnect()
 
-    #Initialized and starting new client for multiple subscribe
+
+
+board_number = len(board_list)
+
+if (board_number == 0):
+
+    print("No board answered")
+
+    exit()
+
+else:
+
+    print(str(board_number) + " boards answered")
+
+
+
+#initialize database
+
+init_db();
+
+
+
+print("running...")
+
+
+
+#initialized and starting new client for multiple subscriptions
+
 client = paho.Client()
 client.username_pw_set("issia", "cnr")
 client.connect("150.145.127.45", 8883)
 client.subscribe("+/data", qos=0)
 client.subscribe("+/hello", qos=0)
+
 client.message_callback_add("+/data", store_data)
 client.message_callback_add("+/hello", welcome)
 client.loop_forever()
